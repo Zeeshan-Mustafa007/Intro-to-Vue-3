8192bf65a5d2ea1c9ee5c8815332f26a7956ba5d
Zeeshan Mustafa <mustafazeeshan007@gmail.com>
2024-01-15 20:38:16 +0500

    Refactor ProductDisplay component and update cart functionality

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
-        }
+            cart: [],
+            premium: true,
+        };
     },
-    methods: {}
-})
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        removeCart(id) {
+            for (let i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                    this.cart.splice(i, 1);
+                    break;
+                }
+            }
+        },
+    },
+});
